5cdbd7d6b6c084a67150973d193433caf6155219
Antony Ferrari Gerevini <antonyfgerevini05@gmail.com>
2024-04-18 22:05:32 -0300

    Finalizado

--- crud.js
+++ crud.js
@@ -65,7 +65,7 @@ function cadastrarPessoa(novaPessoa) {
 }
 
 function deletarPessoa(id) {
-
+    pessoas = pessoas.filter(pessoa => pessoa.id !== id);
 }
 
 function atualizarPessoa(id, dados) {
@@ -81,8 +81,7 @@ function listarPessoas() {
 
 cadastrarPessoa({nome: "Beto", idade: 55, cidade: "São José dos Campos"});
 cadastrarPessoa({nome: "Metilde", idade: 54, cidade: "Saúde"});
-console.log("Lista após criação de duas pessoas:");
-console.log(listarPessoas());
+
 
 atualizarPessoa(pessoas[0].id, {cidade: "Saúde"});
 console.log("Lista após atualização de uma pessoa:");
